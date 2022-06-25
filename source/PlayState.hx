88aa3ddf85974ee6d1c36ed4233e23454b2ff469
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-06-25 03:23:05 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1469,17 +1469,19 @@ class PlayState extends MusicBeatState
 			return;
 		}
 
-		FlxG.sound.music.stop();
+		//FlxG.sound.music.stop();
 		var video:MP4Handler = new MP4Handler();
 		video.playVideo(filepath);
 
 		video.finishCallback = function()
 		{
 			startAndEnd();
+			return;
 		}
 		#else
 		FlxG.log.warn('Platform not supported!');  
 		startAndEnd();
+		return;
 		#end
 	}
 
