fef4e97bbc96e3d752376aafdbf9898d106c6d74
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-08-02 01:50:09 +0300

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -8,7 +8,6 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.addons.display.FlxGridOverlay;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.addons.transition.FlxTransitionableState;
 import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
@@ -158,8 +157,9 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		#if android
 		addVirtualPad(LEFT_FULL, A_B_C);
-		addPadCamera();
-		#end
+	        //addPadCamera();
+	        #end
+
 	}
 
 	var nextAccept:Int = 5;
@@ -178,11 +178,11 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		if (controls.BACK) {
 			#if android
-                        FlxTransitionableState.skipNextTransOut = true;
+			flixel.addons.transition.FlxTransitionableState.skipNextTransOut = true;
 			FlxG.resetState();
-                        #else
-                        close();
-                        #end
+	            	#else
+			close();
+	            	#end
 			ClientPrefs.saveSettings();
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 		}
@@ -296,7 +296,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 				}
 			}
 
-			if(controls.RESET #if android || virtualpad.buttonC.justPressed #end)
+			if(controls.RESET #if android || virtualPad.buttonC.justPressed #end)
 			{
 				for (i in 0...optionsArray.length)
 				{
