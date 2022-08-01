047012052b0920568d4a715b920c2a8b17e7f51d
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-08-01 15:45:53 +0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -186,8 +186,7 @@ class StoryMenuState extends MusicBeatState
 		changeDifficulty();
 
 		#if android
-		addVirtualPad(FULL, A_B_X_Y);
-		addPadCamera();
+		addVirtualPad(LEFT_FULL, A_B_X_Y);
 		#end
 
 		super.create();
@@ -225,6 +224,15 @@ class StoryMenuState extends MusicBeatState
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
 
+			#if !android
+			if(FlxG.mouse.wheel != 0)
+			{
+				FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+				changeWeek(-FlxG.mouse.wheel);
+				changeDifficulty();
+			}
+			#end
+
 			if (controls.UI_RIGHT)
 				rightArrow.animation.play('press')
 			else
@@ -242,7 +250,7 @@ class StoryMenuState extends MusicBeatState
 			else if (upP || downP)
 				changeDifficulty();
 
-			if(FlxG.keys.justPressed.CONTROL #if android || _virtualpad.buttonX.justPressed #end)
+			if(FlxG.keys.justPressed.CONTROL #if android || virtualPad.buttonX.justPressed #end)
 			{
 				#if android
 				removeVirtualPad();
@@ -250,7 +258,7 @@ class StoryMenuState extends MusicBeatState
 				persistentUpdate = false;
 				openSubState(new GameplayChangersSubstate());
 			}
-			else if(controls.RESET #if android || _virtualpad.buttonY.justPressed #end)
+			else if(controls.RESET #if android || virtualPad.buttonY.justPressed #end)
 			{
 				#if android
 				removeVirtualPad();
