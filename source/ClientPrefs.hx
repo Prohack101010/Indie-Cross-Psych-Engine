4eeab5a9a4648bd1473c70cfefd543a9aa6f6fa7
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-08-08 00:20:52 +0300

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -9,6 +9,8 @@ import Controls;
 class ClientPrefs {
 	public static var downScroll:Bool = false;
 	public static var middleScroll:Bool = false;
+	public static var specialNotes:Bool = true;
+	public static var shaders:Bool = true
 	public static var showFPS:Bool = #if android false #else true #end;
 	public static var flashing:Bool = true;
 	public static var globalAntialiasing:Bool = true;
@@ -97,6 +99,8 @@ class ClientPrefs {
 	public static function saveSettings() {
 		FlxG.save.data.downScroll = downScroll;
 		FlxG.save.data.middleScroll = middleScroll;
+		FlxG.save.data.specialNotes = specialNotes;
+		Flxg.save.data.shader = shaders;
 		FlxG.save.data.showFPS = showFPS;
 		FlxG.save.data.flashing = flashing;
 		FlxG.save.data.globalAntialiasing = globalAntialiasing;
@@ -149,6 +153,12 @@ class ClientPrefs {
 		}
 		if(FlxG.save.data.middleScroll != null) {
 			middleScroll = FlxG.save.data.middleScroll;
+		}
+		if(flxG.save.data.specialNotes! = null) {
+			specialNotes = FlxG.save.data.specialNotes;
+		}
+		if(flxG.save.data.shaders! = null) {
+			shaders = FlxG.save.data.shaders;
 		}
 		if(FlxG.save.data.showFPS != null) {
 			showFPS = FlxG.save.data.showFPS;
