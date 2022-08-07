ff9f65964d9a264fb0d28d3104f769eb5bc3931e
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-08-08 01:58:14 +0300

    update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -433,12 +433,35 @@ class FunkinLua {
 			luaTrace("Object " + obj + " doesn't exist!");
 		});
 		Lua_helper.add_callback(lua, "krTween", function(amt:Float) {
-		PlayState.krTween(amt);
+		if (health <= 0)
+			amt = Math.abs(amt);
+		krTweenObj.cancel();
+		krTweenObj = FlxTween.num(kr, kr - amt, 0.1, {ease: FlxEase.cubeInOut}, function(v:Float)
+		{
+			kr = v;
+			PlayState.updatesansbars();
+		});
+	}
+		//PlayState.krTween(amt);
 				});
 			Lua_helper.add_callback(lua, "krChange", function(amt:Float, force:Bool = false) {
 
-		PlayState.krChange(amt, force);
+		if (health <= 0)
+		{
+			amt = Math.abs(amt);
+		}
+		
+		if (krTweenObj!=null)
+			krTweenObj.cancel();
 
+		if (force)
+			kr = amt;
+		else
+			kr -= amt;
+
+		PlayState.updatesansbars();
+	}
+//		PlayState.krChange(amt, force);
 				});
 		// gay ass tweens
 		Lua_helper.add_callback(lua, "doTweenX", function(tag:String, vars:String, value:Dynamic, duration:Float, ease:String) {
