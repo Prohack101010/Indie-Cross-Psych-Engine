aa757aec1c5f9098dc93eee70d4d3f2d75c41460
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-05 14:39:18 +0300

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -200,14 +200,10 @@ class MusicBeatState extends FlxUIState
 		if(!FlxTransitionableState.skipNextTransIn) {
 			leState.openSubState(new CustomFadeTransition(0.6, false));
 			if(nextState == FlxG.state) {
-				CustomFadeTransition.finishCallback = function() {
 					FlxG.resetState();
-				};
 				//trace('resetted');
 			} else {
-				CustomFadeTransition.finishCallback = function() {
 					FlxG.switchState(nextState);
-				};
 				//trace('changed state');
 			}
 			return;
