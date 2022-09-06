e3560227a4939ea8289e0cf70fd820b2ab289310
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-06 20:18:10 +0300

    Thx peppy

--- source/K.hx
+++ source/K.hx
@@ -47,7 +47,7 @@ public static function switchTheFuckingState(balls:FlxState)
   {
     switchTo(balls);
   }
-	static public function switchTo(state:FlxState)
+	public static function switchTo(state:FlxState)
 	{
 		if (!finishedTransOut && !transOutRequested)
 		{
