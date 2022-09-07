39b8185031f312db11ebc7663ba7f46b96c2cea0
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-07 14:32:56 +0300

    update CustomFadeTransition.hx

--- source/CustomFadeTransition.hx
+++ source/CustomFadeTransition.hx
@@ -15,7 +15,7 @@ class CustomFadeTransition extends FlxSubState
 	var rect:FlxSprite;
 	var tween:FlxTween;
 	public static var nextCamera:FlxCamera;
-	public static var finishCallback:Void->Void;
+	public static var finishCallback;
 	var duration:Float;
 
 	var fi:Bool = true;
