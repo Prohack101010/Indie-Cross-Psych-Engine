49f79aaf592ab51e0fdcf1ad5ccdcf37e8204f92
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-09 15:32:36 +0300

    update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -506,7 +506,7 @@ public function addShaderToCamera(cam:String,effect:ShaderEffect){//STOLE FROM A
 			FlxG.sound.play(Paths.sound('scrollMenu'));
 		}
 		super.update(elapsed);
-	Shaders.ChromaticAberrationShader.setChrome(chromVal);
+	shader_chromatic_abberationr.setChrome(chromVal);
 	for (i in shaderUpdates){
 			i(elapsed);
 		}
