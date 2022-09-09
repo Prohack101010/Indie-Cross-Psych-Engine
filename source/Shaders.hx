f95699aede9c3cd77d6dba2b9fe6e0c50aed53bd
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-09 15:48:43 +0300

    update Shaders.hx

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -78,15 +78,15 @@ class ChromaticAberrationShader extends FlxShader
 
 class ChromaticAberrationEffect extends Effect
 {
-	public var shader:ChromaticAberrationShader;
-  public function new(offset:Float = 0.00){
+	public static var shader:ChromaticAberrationShader;
+  public static function new(offset:Float = 0.00){
 	shader = new ChromaticAberrationShader();
     shader.rOffset.value = [offset];
     shader.gOffset.value = [0.0];
     shader.bOffset.value = [-offset];
   }
 	
-	public function setChrome(chromeOffset:Float):Void
+	public static function setChrome(chromeOffset:Float):Void
 	{
 		shader.rOffset.value = [chromeOffset];
 		shader.gOffset.value = [0.0];
