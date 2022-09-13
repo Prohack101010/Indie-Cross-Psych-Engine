b5db2f44477806df0633f448b673e6eeec22aee1
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-13 04:22:55 +0300

    update ChromaticAberration.hx

--- source/ChromaticAberration.hx
+++ source/ChromaticAberration.hx
@@ -5,13 +5,11 @@ import Shaders;
 
 class ChromaticAberration extends Shaders
 {
-	public static var chromaticAberration:ShaderFilter = new ShaderFilter(new ChromaticAberration());
- 	public function new(){
-	chromaticAberration = new ChromaticAberrationShader();
-		chromaticAberrationShader.shader.data.rOffset.value = [offset];
-		chromaticAberrationShader.shader.data.gOffset.value = [0.0];
-		chromaticAberrationShader.shader.data.bOffset.value = [-offset];
-  }
+	public static var chromaticAberration:ShaderFilter = new ShaderFilter(new ChromaticAberration(offset)) {
+    chromaticAberration.rOffset.value = [offset];
+    chromaticAberration.gOffset.value = [0.0];
+    chromaticAberration.bOffset.value = [-offset];
+	}
 	public static function setChrome(chromeOffset:Float):Void
 	{
 		chromaticAberrationShader.shader.data.rOffset.value = [chromeOffset];
