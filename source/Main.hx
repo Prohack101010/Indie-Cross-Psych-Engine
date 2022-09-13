527f9f978b2b732cf0d18c01ce47029e4837df74
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-13 21:21:30 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -8,7 +8,7 @@ import flixel.FlxGame;
 import flixel.FlxState;
 import openfl.Assets;
 import openfl.Lib;
-import FPS;
+import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
