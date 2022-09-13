f768fc8bf56ac33de8b794eb7c9704e03f6b62da
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-13 08:12:47 +0300

    update ChromaticAberration.hx

--- source/ChromaticAberration.hx
+++ source/ChromaticAberration.hx
@@ -1,16 +1,31 @@
 package;
 
-import openfl.filters.ShaderFilter;
-import Shaders;
+import flixel.system.FlxAssets.FlxShader;
 
-class ChromaticAberration extends Shaders
+class ChromaticAberration extends FlxShader
 {
-	public static var chromaticAberration:ShaderFilter = new ShaderFilter(new ChromaticAberration());
+	@:glFragmentSource('
+		#pragma header
 
-	public static function setChrome(chromeOffset:Float):Void
+		uniform float rOffset;
+		uniform float gOffset;
+		uniform float bOffset;
+
+		void main()
+		{
+			vec4 col1 = texture2D(bitmap, openfl_TextureCoordv.st - vec2(rOffset, 0.0));
+			vec4 col2 = texture2D(bitmap, openfl_TextureCoordv.st - vec2(gOffset, 0.0));
+			vec4 col3 = texture2D(bitmap, openfl_TextureCoordv.st - vec2(bOffset, 0.0));
+			vec4 toUse = texture2D(bitmap, openfl_TextureCoordv);
+			toUse.r = col1.r;
+			toUse.g = col2.g;
+			toUse.b = col3.b;
+			//float someshit = col4.r + col4.g + col4.b;
+
+			gl_FragColor = toUse;
+		}')
+	public function new()
 	{
-		chromaticAberrationShader.shader.data.rOffset.value = [chromeOffset];
-		chromaticAberrationShader.shader.data.gOffset.value = [0.0];
-		chromaticAberrationShader.shader.data.bOffset.value = [chromeOffset * -1];
+		super();
 	}
 }
