ac98ab2a01332ec5b22d80a94a50cf8c27118499
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2022-09-24 15:46:02 +0300

    Update FlashingState.hx

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -26,11 +26,11 @@ class FlashingState extends MusicBeatState
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Hey, watch out!\n
 			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
+			Press ESCAPE to disable them now or go to Options Menu.\n
+			Press ENTER to ignore this message.\n
 			You've been warned!",
 			32);
-		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		warnText.setFormat("Bronx.otf", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
 
@@ -48,8 +48,6 @@ class FlashingState extends MusicBeatState
 				FlxTransitionableState.skipNextTransIn = true;
 				FlxTransitionableState.skipNextTransOut = true;
 				if(!back) {
-					ClientPrefs.flashing = false;
-					ClientPrefs.saveSettings();
 					FlxG.sound.play(Paths.sound('confirmMenu'));
 					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
 						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
@@ -57,10 +55,12 @@ class FlashingState extends MusicBeatState
 						});
 					});
 				} else {
+					ClientPrefs.flashing = false;
+					ClientPrefs.saveSettings();
 					FlxG.sound.play(Paths.sound('cancelMenu'));
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
 						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new TitleState());
+							MusicBeatState.switchState(new MainMenuState()); //incase the crash is from title state
 						}
 					});
 				}
