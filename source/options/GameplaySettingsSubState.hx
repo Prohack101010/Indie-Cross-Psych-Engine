4b2670d2ff6e1a213a34fd1561bb028e9155d407
An-undertale-fan <94998581+An-undertale-fan@users.noreply.github.com>
2023-02-11 14:31:19 +0200

    Update GameplaySettingsSubState.hx

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -25,7 +25,7 @@ import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import Controls;
 #if android
-import android.Hardware;
+import android.Tools;
 #end
 
 using StringTools;
@@ -174,7 +174,7 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 	{
 		if(ClientPrefs.vibration)
 		{
-			Hardware.vibrate(500);
+			Tools.vibrate(500);
 		}
 	}
 	#end
